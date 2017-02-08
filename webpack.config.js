c559ace36673077db30960be8e7f8c7e85e0358d
valcol <valcol.nc@gmail.com>
2017-02-08 06:09:15 +0100

    feat(all): add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+var path = require('path');
+
+var BUILD_DIR = path.resolve(__dirname, 'public');
+var APP_DIR = path.resolve(__dirname, 'view');
+
+var config = {
+  entry: APP_DIR + '/App.jsx',
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js'
+  },
+  module : {
+    loaders : [
+      {
+        test : /\.jsx?/,
+        include : APP_DIR,
+        loader : 'babel-loader'
+      },
+      {
+        test : /\.less?/,
+        include : APP_DIR,
+        loader : "style-loader!css-loader!less-loader"
+      }
+    ]
+  },
+  watch: false
+};
+
+module.exports = config;
