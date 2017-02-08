f44d6d88eea98bf9d5dd31446a8989dbe0723830
valcol <valcol.nc@gmail.com>
2017-02-08 06:58:41 +0100

    feat(all): add test framework

---
+++ test/test.js
@@ -0,0 +1,10 @@
+'use strict';
+
+let assert = require('assert');
+
+describe('#funtionToTest()', function () {
+    it('should return something', function () {
+      let something = true;
+      assert.ok(something, 'OK');
+    });
+});
