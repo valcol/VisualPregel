42af1e8fe8a2ca091a3c9bf95da6005a8efd1891
valcol <valcol.nc@gmail.com>
2017-02-10 20:44:38 +0100

    feat(Helpers): add function to string helper

---
+++ controllers/Helpers/index.js
@@ -0,0 +1,7 @@
+let Helpers = function() {};
+
+Helpers.prototype.functionToString = function(f) {
+  return f.toSting();
+};
+
+export default new Helpers();
--- controllers/Pregel/index.js
+++ controllers/Pregel/index.js
@@ -2,14 +2,14 @@ let Pregel = function() {};
 
 Pregel.prototype.initialize = function() {
  //init function here
-}
+};
 
 Pregel.prototype.dispatch = function() {
   //dispatch function here
-}
+};
 
 Pregel.prototype.aggregate = function() {
   //aggregate function here
-}
+};
 
 export default new Pregel();
