f416cecdafe1a14fa0e2560c9562b938f38bb802
valcol <valcol.nc@gmail.com>
2017-02-10 20:48:02 +0100

    feat(Helpers): add basic string to function helper

--- controllers/Helpers/index.js
+++ controllers/Helpers/index.js
@@ -1,5 +1,12 @@
 let Helpers = function() {};
 
+
+Helpers.prototype.stringToFunction = function(s) {
+  let f;
+  eval('f = ' + s);
+  return f;
+};
+
 Helpers.prototype.functionToString = function(f) {
   return f.toSting();
 };
