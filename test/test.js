2d696b4d586f20cc0c7e716a478189df9d958b71
valcol <valcol.nc@gmail.com>
2017-02-11 02:18:27 +0100

    feat(tests): add unit tests

--- test/test.js
+++ test/test.js
@@ -1,10 +1,20 @@
 'use strict';
 
 let assert = require('assert');
+import Helpers from '../controllers/Helpers/index.js';
 
-describe('#funtionToTest()', function () {
-    it('should return something', function () {
-      let something = true;
-      assert.ok(something, 'OK');
+describe('stringToFunction()', function () {
+    it('should convert a string into a function', function () {
+      let s = 'function (value) {return value * 2;}';
+      let f = Helpers.stringToFunction(s);
+      assert.ok(f(2), 4);
+    });
+});
+
+describe('functionToString()', function () {
+    it('should convert a function into a string', function () {
+      let f = function(value) {return value * 2;};
+      let s = Helpers.functionToString(f);
+      assert.ok(s, 'function f(value) {return value * 2;}');
     });
 });
