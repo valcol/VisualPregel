5a15cc05d2631d75e97e16e111c0fae0cb403e50
Florian Berthelot <fberthel@emi.u-bordeaux1.fr>
2017-02-13 18:12:53 +0100

    modification of the graph's structure, handling multiple errors in csv file

--- controllers/FileHandler/index.js
+++ controllers/FileHandler/index.js
@@ -1,20 +1,18 @@
-
-
 let FileHandler = function() {
-	this.listOfNodes = [];
+	this.listOfNodes = {};
 };
 
 FileHandler.prototype.fileToGraph = function(file, updateBar){
 	let now = 0;
+	FileHandler.listOfNodes = {};
 	if(file == undefined)
-		return;
-	var filename = file.name.split('.');
+	return;
+	var filename = file.name.split(".");
 	var fileformat = filename[filename.length-1];
 	if(!fileformat.includes("csv")){
-		listOfNodes = [];
 		updateBar(now,"danger");
 		alert("Your file is not csv file");
-		file.value = '';
+		file.value = "";
 		return;
 	}
 	var reader = new FileReader();
@@ -22,31 +20,37 @@ FileHandler.prototype.fileToGraph = function(file, updateBar){
 	var line = "";
 	var lines = [];
 	reader.onload = function(evt){
-		lines = this.result.split('\n');
+		lines = this.result.split("\n");
 		for(var i = 0; i < lines.length; i++){
-			line = lines[i].split(',');
+			line = lines[i].split(/,| |\t/);
 			if(!edges){
 				if(lines[i].includes("EDGES")){
 					edges = true;
 					continue;
 				}
-				FileHandler.listOfNodes.push({
+				FileHandler.listOfNodes[parseInt(line[0])] = {
 					id: parseInt(line[0]),
 					value: parseInt(line[1]),
 					listOfNeighbours: []
-				});
+				};
 			}
 			else{
-				var nodeID = parseInt(line[0]);
+				let nodeID = parseInt(line[0]);
+				if(FileHandler.listOfNodes[nodeID] != null)
 				for(var j = 1; j < line.length; j++){
-					var actualNode = FileHandler.listOfNodes[nodeID]
-					if(actualNode != null)
-						actualNode.listOfNeighbours.push(parseInt(line[j]));
+					let neighbourNode = parseInt(line[j]);
+					if(FileHandler.listOfNodes[neighbourNode] != null )
+					FileHandler.listOfNodes[nodeID].listOfNeighbours.push(neighbourNode);
 					else{
-						alert("Fail to read csv file.\nAn error occurs line " + (i+1));
 						updateBar(now,"danger");
+						alert("Fail to read csv file.\nAn error occurs line " + (i+1));
 						return;
 					}
+				}
+				else{
+					updateBar(now,"danger");
+					alert("Fail to read csv file.\nAn error occurs line " + (i+1));
+					return;
 				}
 			}
 			now = 100/(lines.length - i);
