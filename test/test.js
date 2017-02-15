989405921509f04be100d9f6f62a4acd8e815b1b
tinouS <tinoudi.simohammed@gmail.com>
2017-02-15 21:33:18 +0100

    add function to create 2 test files

--- test/test.js
+++ test/test.js
@@ -1,6 +1,8 @@
 'use strict';
 
 let assert = require('assert');
+let fs = require('fs');
+
 import Helpers from '../controllers/Helpers/index.js';
 
 describe('stringToFunction()', function () {
@@ -17,4 +19,24 @@ describe('functionToString()', function () {
       let s = Helpers.functionToString(f);
       assert.ok(s, 'function f(value) {return value * 2;}');
     });
+});
+
+/**
+function to create 2 test files
+---- To modify: content of goodValues and badValues. ----
+**/
+
+describe('CreateTextFile',function(){
+  let goodValues = '0,3 \n3,2\n';
+  let badValues = '4,1 \n4,9\n';
+  let values= [goodValues,badValues];
+  console.log(values);
+  it('create file ',function(){
+    for (var i = 0; i < 2; i++) {
+      fs.writeFile('testFile'+i+'.csv',values[i]+'\n', function (err) {
+          if (err) throw err;
+          console.log('Files created');
+      });
+    }
+  });
 });
