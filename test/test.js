8566ec8de99fd344e03d13d77402d6f42d71ae62
tinouS <tinoudi.simohammed@gmail.com>
2017-03-07 18:27:20 +0100

    add test: dispach() from pregel. US4

--- test/test.js
+++ test/test.js
@@ -5,6 +5,7 @@ let fs = require('fs');
 
 import Helpers from '../controllers/Helpers/index.js';
 import FileHandler from '../controllers/FileHandler/index.js';
+import Pregel from '../controllers/Pregel/index.js';
 
 describe('stringToFunction()', function () {
   it('should convert a string into a function', function () {
@@ -52,7 +53,8 @@ describe('parsingGraph()',function(){
   };
   it('should parse a CSV file and output a graph object with mistakes',function(){
     let update = function(val1,val2){};
-    let callback = function(val1,val2){};
+    let callback = function(val1,val2){
+    };
     FileHandler.parsingGraph(values, ',', update,
       (n,m,x) => {assert.deepEqual(m, nodesFinal);} , callback
     );
@@ -74,6 +76,31 @@ describe('parsingGraph()',function(){
       (n,m,x) => {assert.deepEqual(m, nodesFinal);} , callback
     );
   });
+
+// TO CONTINUE
+    /*
+    values = "8,4,7\n7,1\n1,2\n2,5\n1,8\n7,5\n";
+    nodesFinal = {
+        '1': { value: 1, initialValue: -1 },
+        '2': { value: 2, initialValue: -1 },
+        '4': { value: 4, initialValue: -1 },
+        '5': { value: 5, initialValue: -1 },
+        '7': { value: 7, initialValue: -1 },
+        '8': { value: 8, initialValue: -1 }
+    };
+    let count = 0;
+    it('should test number ',function(){
+        let update = function(val1,val2){};
+        let setError= function(val1,val2){
+            count++;
+        };
+        console.log(count);
+        FileHandler.parsingGraph(values, ',', update,
+            (n,m,x) => {assert.deepEqual(m, nodesFinal);} , setError
+        );
+    });
+*/
+
 });
 
 // parsing test of fileToGraph function.
@@ -102,3 +129,21 @@ describe('parsingValues()',function(){
     );
   });
 });
+
+//
+describe('dispatch()',function(){
+    let srcId = 1 , srcAttr = 10 ;
+    it('test dispatch function', function () {
+        let dispatch = Pregel.dispatch(srcId,srcAttr);
+        let result = 10;
+       assert.equal(dispatch,result);
+    });
+});
+
+/*
+describe('aggregate()',function(){
+    it('', function () {
+
+    });
+});
+*/
