80972c3c5a7206e70b20bdfebad9827080947e4b
slassouani <lassouani_sofiane@outlook.com>
2017-03-09 12:31:23 +0100

    Ajout de 3 boutons de control de l'algo Pregel

--- components/Panel.js
+++ components/Panel.js
@@ -3,14 +3,33 @@ import UploadGraphBox from '../containers/UploadGraphBox';
 import UploadValuesBox from '../containers/UploadValuesBox';
 import { Button } from 'react-bootstrap';
 import PregelMock from '../containers/PregelMock';
+import { FormGroup, InputGroup, FormControl } from 'react-bootstrap';
+import { ButtonToolbar, ButtonGroup, Glyphicon, span } from 'react-bootstrap';
 
-const Panel = ({setRandomNodes, setPregelMockFunction}) => {
+const Panel = ({setRandomNodes, setPregelMockFunction, Return, Play, Stop}) => {
   return (
     <div className='panel'>
     <UploadGraphBox idName = "graph" label = "Upload graph file :"/>
     <UploadValuesBox idName="values" label="Upload values file :"/>
     <Button bsSize="large" onClick={setRandomNodes} block>Generate random graph</Button>
     <PregelMock pregelMock = {setPregelMockFunction}/>
+
+    <div className='panel'>
+       <ButtonToolbar>
+           <ButtonGroup>
+             <Button bsSize="lg" eventKey='1' onClick={Return}>
+               <span class="glyphicon glyphicon-triangle-left" aria-label="Left Align"></span> Return
+             </Button>
+             <Button bsSize="lg" eventKey='2' onClick={Stop}>
+               <span class="glyphicons glyphicons-pause" aria-label="Left Align"></span> Stop
+             </Button>
+             <Button bsSize="lg" eventKey='3' onClick={Play}>
+               <span class="glyphicons glyphicons-play" aria-label="Left Align"></span> Play
+             </Button>
+           </ButtonGroup>
+      </ButtonToolbar>
+   </div>
+
     </div>
   );
 }
