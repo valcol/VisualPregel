01f3ca14a7350ee48870552abf967bc4907cc365
chaimaeelassaoui <chaimae.elassaoui@gmail.com>
2017-03-16 15:54:32 +0100

    add valuedrop for state later

--- components/Panel.js
+++ components/Panel.js
@@ -6,8 +6,9 @@ import PregelMock from '../containers/PregelMock';
 import { FormGroup, InputGroup, FormControl } from 'react-bootstrap';
 import { ButtonToolbar, ButtonGroup, Glyphicon, span } from 'react-bootstrap';
 import RangeSlider from '../containers/RangeSlider';
+import ValueDropDown from './ValueDropDown';
 
-const Panel = ({setRandomNodes, setPregelMockFunction, BackFast, Back, Play, ForwardFast, Forward}) => {
+const Panel = ({setRandomNodes, setPregelMockFunction, BackFast, Back, Play, ForwardFast, Forward, value, setValue}) => {
   return (
     <div className='panel'>
     <UploadGraphBox idName = "graph" label = "Upload graph file :"/>
@@ -15,27 +16,8 @@ const Panel = ({setRandomNodes, setPregelMockFunction, BackFast, Back, Play, For
     <Button bsSize="large" onClick={setRandomNodes} block>Generate random graph</Button>
     <PregelMock pregelMock = {setPregelMockFunction}/>
 
-
-
-    <FormGroup controlId="formControlsSelectValue">
-      <InputGroup>
-     <InputGroup.Addon>Select value :</InputGroup.Addon>
-      <FormControl componentClass="select" placeholder="select" >
-        <option value="1">1</option>
-        <option value="2">2</option>
-        <option value="3">3</option>
-        <option value="4">4</option>
-        <option value="5">5</option>
-        <option value="6">6</option>
-        <option value="7">7</option>
-        <option value="8">8</option>
-        <option value="9">9</option>
-        <option value="10">10</option>
-      </FormControl>
-    </InputGroup>
-    </FormGroup>
-
-
+    
+    <ValueDropDown setValue={setValue} value={value}/>
 
        <ButtonToolbar>
            <ButtonGroup>
